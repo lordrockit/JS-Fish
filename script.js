740d38650467f469e222a4a0ee08ad361c5f536e
lordrockit <spbeau@gmail.com>
2014-05-15 20:06:03 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ var ctx = c.getContext("2d");
 var x = 50;
 var y = 50;
 
-var width = screen.width-50/2;
-var height = screen.height-100/2;
+var width = (screen.width-50)/2;
+var height = (screen.height-100)/2;
 
 var imageObj = new Image();
 
